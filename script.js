a5983f99e92d00485f4e698f6811e0977342ac9d
Faizy179 <faizy_a12@hotmail.com>
2026-07-04 18:56:11 -0400

    Added bullets that shoot once the player is lined up with the enemy

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ const player = {
 }
 let  enemies = [];
 let isThrusting = false;
+let bullets = [];
+let lastShotTime = 0;
+const SHOOT_COOLDOWN = 1000; //1 second cooldown
+
 
 document.addEventListener(
     "keydown",(e) => {
@@ -28,7 +32,7 @@ document.addEventListener(
     }
 );
 
-function updateLogic(){
+function updateLogic(currentTime){
     if(isThrusting){
         player.x += Math.cos(player.angle) * player.speed;
         player.y += Math.sin(player.angle) *player.speed;
@@ -47,8 +51,50 @@ function updateLogic(){
             enemy.y += Math.sin(angleToPlayere) * enemy.speed;
         }
     );
+    if((currentTime - lastShotTime) >= SHOOT_COOLDOWN && enemies.length >0){
+        let nearestEnemy = enemies[0];
+        let minDistanc = Infinity;
+        enemies.forEach(
+            enemy => {
+                const dist = Math.hypot(player.x-enemy.x,player.y-enemy.y);
+                if(dist < minDistanc){
+                    minDistanc = dist;
+                    nearestEnemy = enemy;
+                }
+            }
+        );
+        const angleToEnemey = Math.atan2(nearestEnemy.y-player.y, nearestEnemy.x-player.x);
+        bullets.push(
+            {
+                x: player.x,
+                y: player.y,
+                radius: 5,
+                speed:10,
+                angle: angleToEnemey,
+                color: "yellow"
+            }
+        );
+        lastShotTime = currentTime;
+    }
+    bullets.forEach(
+        bullet =>{
+            bullet.x += Math.cos(bullet.angle) *bullet.speed;
+            bullet.y += Math.sin(bullet.angle) * bullet.speed;
+        }
+    );
+    for(let i= bullets.length-1; i>=0; i--){
+        for(let j = enemies.length-1; j>=0;j--){
+            const b = bullets[i];
+            const e = enemies[j];
+            const dist = Math.hypot(b.x-e.x,b.y-e.y);
+            if(dist < b.radius+e.radius){
+                bullets.splice(i,1);
+                enemies.splice(j,1);
+                break;
+            }
+        }
+    }
 }
-
 function render(interp){
     ctx.clearRect(0,0,canvas.width,canvas.height);
     enemies.forEach(
@@ -56,6 +102,14 @@ function render(interp){
             ctx.fillStyle = enemy.color;
             ctx.beginPath();
             ctx.arc(enemy.x,enemy.y,enemy.radius,0,(Math.PI * 2));
+            ctx.fill();
+        }
+    );
+    bullets.forEach(
+        bullet => {
+            ctx.fillStyle = bullet.color;
+            ctx.beginPath();
+            ctx.arc(bullet.x,bullet.y,bullet.radius,0,Math.PI*2);
             ctx.fill();
         }
     );
@@ -85,13 +139,12 @@ function gameLoop(currentTime){
         delta  = 1;
     }
     while(delta >= 1){
-        updateLogic();
+        updateLogic(currentTime);
         delta--;
     }
     render(delta);
     requestAnimationFrame(gameLoop);
 }
-
 
 enemies.push({ x: 50, y: 50, radius: 12, speed: 2, color: "red" });
 requestAnimationFrame(gameLoop);
